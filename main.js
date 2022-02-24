50ac897acc40421e1004fa75f2a6ade04a36755c
erich-m <erich.m.maclean@gmail.com>
2022-02-24 03:07:00 -0500

    added content files, added project details 2 site

--- main.js
+++ main.js
@@ -4,8 +4,16 @@ let textColor;
 
 let mode = 0;
 let counter = 0;
+let change = 0.5;
 
 let nameFont;
+
+let toggleAnimation = 1;
+let center, r;
+
+function preload(){
+	nameFont = loadFont("assets/headerfont.ttf");
+}
 
 function setup() {
 	let headerSize = (document.getElementById("header")).getBoundingClientRect();
@@ -13,7 +21,6 @@ function setup() {
 	cnv = createCanvas(canvasSize.width, canvasSize.height);
 	cnv.position(0,headerSize.height);
 	
-	nameFont = loadFont("assets/headerfont.ttf");
 
 	backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--background-color');
 	textColor = getComputedStyle(document.documentElement).getPropertyValue('--contrast-text');
@@ -23,10 +30,11 @@ function draw(){
 	background(backgroundColor);
 	// line(0,cnv.height/2,cnv.width,cnv.height/2);
 	if(mode == 0){
-		let center = createVector(cnv.width/6,cnv.height/2);
-		let r = cnv.height*0.75/2;
-		noStroke(0);
-		fill(100);
+		center = createVector(cnv.width*0.2,cnv.height/2);
+		r = cnv.height*0.75/2;
+
+		noStroke();
+		fill(10);
 		ellipse(center.x,center.y,r*2,r*2);
 		let minorOffset = counter;
 		let colorArray = ['#FF0000','#FF8700','#FFD300','#DEFF0A','#A1FF0A','#0AFF99','#0AEFFF','#147DF5','#580AFF','#BE0AFF'];
@@ -35,22 +43,28 @@ function draw(){
 			drawArc(counter+majorOffset,counter+minorOffset+majorOffset,0.5*abs(sin(minorOffset)),r*sin(minorOffset)*0.8,center.x,center.y,r,colorArray[a]);
 		}
 		noFill();
-		strokeWeight(10);
+		strokeWeight(11);
 		stroke(0);
 		ellipse(center.x,center.y,r*2,r*2);
-		counter+=0.5;
+		if(toggleAnimation > 0){
+			counter+=change;
+		}
 
 		fill(0);
 		stroke(textColor);
-		textSize(300);
+		textSize(windowWidth/6);
 		textFont(nameFont);
 		textAlign(CENTER,CENTER);
-		text("Erich",cnv.width*0.6,cnv.height/2);
+		text("Erich",cnv.width*0.7,cnv.height/2);
 	}
 }
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
+	let headerSize = (document.getElementById("header")).getBoundingClientRect();
+	let canvasSize = (document.getElementById("mainPage")).getBoundingClientRect();
+	cnv = createCanvas(canvasSize.width, canvasSize.height);
+	cnv.position(0,headerSize.height);
 }
 
 
@@ -75,4 +89,12 @@ function drawArc(startAngle,endAngle,mid,dist,x,y,r,col){
 	strokeWeight(15);
 	stroke(0);
 	point(midpoint.x,midpoint.y);
+}
+
+function mousePressed(){
+	if(dist(mouseX,mouseY,center.x,center.y) <= r){
+		toggleAnimation *= -1;
+	}
+
+	change*=toggleAnimation;
 }
